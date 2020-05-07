eae60363892e1b92023a1407963e7e00ee810a3a
Nico <nicopast@ucm.es>
2020-05-07 16:06:45 +0200

    Lo de Ana ya funciona

--- ProyectoDSI/Characters.cs
+++ ProyectoDSI/Characters.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
-namespace MadRacing
+namespace ProyectoDSI
 {
     public class Character
     {
--- ProyectoDSI/LoadOut.xaml.cs
+++ ProyectoDSI/LoadOut.xaml.cs
@@ -23,7 +23,7 @@ using Windows.Gaming.Input;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
-namespace MadRacing
+namespace ProyectoDSI
 {
     /// <summary>
     /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
