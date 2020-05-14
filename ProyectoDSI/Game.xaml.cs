152ad17ce21c964426a90a47534b4fe8a3a8a022
MetalCarlosR <MetalCarlosR@users.noreply.github.com>
2020-05-14 14:55:12 +0200

    Cambios Menu Juego

--- ProyectoDSI/Game.xaml.cs
+++ ProyectoDSI/Game.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -25,6 +27,40 @@ namespace ProyectoDSI
         public Game()
         {
             this.InitializeComponent();
+        }
+
+        private static bool IsEscButtonPressed()
+        {
+            var EscState = Windows.UI.Core.CoreWindow.GetForCurrentThread().GetKeyState(Windows.System.VirtualKey.Escape);
+            return (EscState & Windows.UI.Core.CoreVirtualKeyStates.Down) == Windows.UI.Core.CoreVirtualKeyStates.Down;
+        }
+
+        private void MenuKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (IsEscButtonPressed()) { 
+            
+                if (PauseMenu.Visibility == Visibility.Collapsed)
+                {
+                    PauseMenu.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    PauseMenu.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+        private void OptionsClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Settings));
+        }
+        private void ResumeClick(object sender, RoutedEventArgs e)
+        {
+            PauseMenu.Visibility = Visibility.Collapsed;
         }
     }
 }
