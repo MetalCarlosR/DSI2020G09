3b08e4b3f35c0495678ecaec508b8dd3e9970ad1
Drathijin <rsulbara@ucm.es>
2020-05-14 16:05:34 +0200

    no he compilado, no se si va, pero me la juegoi

--- ProyectoDSI/Player.cs
+++ ProyectoDSI/Player.cs
@@ -52,6 +52,19 @@ namespace ProyectoDSI
         public static void removeFriend(Friend f) { friends_.Remove(f); }
     }
 
+    struct Challenge
+    {
+        public Challenge(string s, int acV, int mV)
+        {
+            message = s;
+            actualValue = acV;
+            maxValue = mV;
+        }
+        public string message;
+        public int actualValue;
+        public int maxValue;
+    }
+
     // Best if singleton
     class Player
     {
@@ -60,10 +73,27 @@ namespace ProyectoDSI
         public static int gold { get; set; } = 1530;
         public static int gems { get; set; } = 565;
 
+        public static List<Challenge> dailyChallenge = new List<Challenge>()
+        {
+            new Challenge("Play one game", 1,1),
+            new Challenge("Drive 10 km",7,10),
+            new Challenge("Kill 10 players", 10,10),
+            new Challenge("Get top 3 in one game", 1,1)
+        };
+
+        public static List<Challenge> weeklyChallenge = new List<Challenge>()
+        {
+            new Challenge("Win 10 games", 9,10),
+            new Challenge("Win 3 Battle Royale games",3,3),
+            new Challenge("Play Driver role in 20 games",2,20),
+            new Challenge("Kill 150 players",134,150)
+        };
+
         public static void addGold(int g) { gold += g; }
         public static void addGems(int g) { gems += g; }
 
         public static void removeGold(int g) { gold -= g; }
         public static void removeGems(int g) { gems -= g; }
+
     }
 }
